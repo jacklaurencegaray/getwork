ccbc0993ed660a1813736639e8363819cc8c92fb
Carlo Janea <janeacarlo@gmail.com>
2018-11-22 10:08:26 +0800

    added navigation for sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './authentication/register/register.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './authentication/login/login.component';
+import { CompanyPageComponent } from './company-page/company-page.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: ':name', component: CompanyPageComponent, pathMatch: 'full' }
+
 ];
 
 @NgModule({
